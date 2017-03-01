f0b4288f977460fe3ddac342bc10717ffba2ff08
OnlyPython <xyfapple@gmail.com>
2017-03-01 17:48:43 +0800

    control transaction by Spring annotation

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dao.StudentDao;
 import entity.Student;
 import utils.Page;
 
 @Service
+@Transactional(readOnly=false)
 public class StudentService {
 	@Resource
 	private StudentDao studentDao;
@@ -30,11 +32,13 @@ public class StudentService {
 			this.studentDao.saveOrUpdateEntity(student);
 		}
 	}
+	@Transactional(readOnly=true)
 	public Page<Student> searchByName(){
 		int currentPage = Page.DEFAULT_CURRENT_PAGE;
 		int numPerPage = Page.DEFAULT_NUM_PER_PAGE;
 		return this.searchByName(null, currentPage, numPerPage);
 	}
+	@Transactional(readOnly=true)
 	public Page<Student> searchByName(String studentName,int currentPage,int numPerPage) {
 		int totalNum = this.studentDao.getTotalNum(studentName);
 		Page<Student> page = new Page<>(currentPage,numPerPage,totalNum);
@@ -45,6 +49,7 @@ public class StudentService {
 	public void deleteById(Integer id) {
 		this.studentDao.deleteById(id);
 	}
+	@Transactional(readOnly=true)
 	public Student getById(Integer id) {
 		return this.studentDao.getById(id);
 	}
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -4,11 +4,13 @@ package service;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dao.UserDao;
 import entity.User;
 
 @Service
+@Transactional(readOnly=true)
 public class UserService {
 	@Resource
 	private UserDao userDao;
